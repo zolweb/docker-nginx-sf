bce90a9bf12a3d6f7a7f4e85f004c4c0c0c9fcbd
Romain Richard <romain@zol.fr>
2016-06-02 14:02:50 +0200

    Remove setfacl

--- 1.0/script/start.sh
+++ 1.0/script/start.sh
@@ -5,9 +5,4 @@ __DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
 __BASE__="$(basename "${0}")"
 __FILE__="${__DIR__}/${__BASE__}"
 
-
-# Give permissions to read and write in workdir for users root and www-data
-setfacl -R -m u:www-data:rwx -m u:root:rwx /var/www
-setfacl -dR -m u:www-data:rwx -m u:root:rwx /var/www
-
 /usr/sbin/nginx
